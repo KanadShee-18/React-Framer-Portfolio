c10c2053315bc45b6850c87a5376df23dca1dcd9
Kanad Shee <codewizardkanad@gmail.com>
2024-07-29 21:42:08 +0530

    Personal Details has been removed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -44,17 +44,9 @@ const Contact = () => {
               whileInView={{ opacity: 1, x: 0 }}
               initial={{ opacity: 0, x: -100 }}
               transition={{ duration: 1 }}
-              className="mb-2"
-            >
-              <FaPaperPlane className="inline mr-2" /> codewizardkanad@gmail.com
-            </motion.p>
-            <motion.p
-              whileInView={{ opacity: 1, x: 0 }}
-              initial={{ opacity: 0, x: 100 }}
-              transition={{ duration: 1 }}
               className="mb-12"
             >
-              <FaPhone className="inline mr-2" /> +91-6297269857
+              <FaPaperPlane className="inline mr-2" /> codewizardkanad@gmail.com
             </motion.p>
             <motion.div
               whileInView={{ opacity: 1, y: 0 }}
