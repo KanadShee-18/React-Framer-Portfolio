bb7d9ac10e101241917166f7bfb43f0e662ecdfc
Kanad Shee <codewizardkanad@gmail.com>
2024-08-25 16:41:30 +0530

    Added hover effect on image

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,68 +4,79 @@ import { FaExternalLinkAlt } from "react-icons/fa";
 import { motion } from "framer-motion";
 
 const Projects = () => {
-  return (
-    <div className="border-b border-neutral-900 pb-4">
-      <motion.h1
-        whileInView={{ opacity: 1, y: 0 }}
-        initial={{ opacity: 0, y: -100 }}
-        transition={{ duration: 0.5 }}
-        className="my-20 text-center text-5xl bg-gradient-to-r from-blue-800 via-blue-300 to-neutral-200 bg-clip-text tracking-tight text-transparent pb-2"
-      >
-        Projects
-      </motion.h1>
-      <div>
-        {PROJECTS.map((project, index) => (
-          <div key={index} className="mb-12 flex flex-wrap lg:justify-center">
-            <motion.div
-              whileInView={{ opacity: 1, x: 0 }}
-              initial={{ opacity: 0, x: -100 }}
-              transition={{ duration: 1 }}
-              className="w-full lg:w-1/4"
+    return (
+        <div className="pb-4 border-b border-neutral-900">
+            <motion.h1
+                whileInView={{ opacity: 1, y: 0 }}
+                initial={{ opacity: 0, y: -100 }}
+                transition={{ duration: 0.5 }}
+                className="pb-2 my-20 text-5xl tracking-tight text-center text-transparent bg-gradient-to-r from-blue-800 via-blue-300 to-neutral-200 bg-clip-text"
             >
-              <img
-                src={project.image}
-                width={200}
-                height={200}
-                alt={project.title}
-                className="mb-6 rounded"
-              />
-            </motion.div>
-            <motion.div
-              whileInView={{ opacity: 1, x: 0 }}
-              initial={{ opacity: 0, x: 100 }}
-              transition={{ duration: 1 }}
-              className="w-full max-w-xl lg:w-3/4"
-            >
-              <h6 className="mb-2 font-semibold text-2xl bg-gradient-to-r from-pink-400 via-slate-500 to-purple-500 bg-clip-text tracking-tight text-transparent cursor-pointer hover:bg-gradient-to-r hover:from-purple-400 hover:via-slate-500 hover:to-pink-500 hover:bg-clip-text hover:tracking-tight hover:text-transparent">
-                {project.title}
-              </h6>
+                Projects
+            </motion.h1>
+            <div>
+                {PROJECTS.map((project, index) => (
+                    <div
+                        key={index}
+                        className="flex flex-wrap mb-12 lg:justify-center"
+                    >
+                        <motion.div
+                            whileInView={{ opacity: 1, x: 0 }}
+                            initial={{ opacity: 0, x: -100 }}
+                            transition={{ duration: 1 }}
+                            className="w-full lg:w-1/4"
+                        >
+                            <a
+                                href={project.link}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <img
+                                    src={project.image}
+                                    width={200}
+                                    height={200}
+                                    alt={project.title}
+                                    className="mb-6 duration-200 ease-in-out transform rounded hover:scale-105"
+                                />
+                            </a>
+                        </motion.div>
+                        <motion.div
+                            whileInView={{ opacity: 1, x: 0 }}
+                            initial={{ opacity: 0, x: 100 }}
+                            transition={{ duration: 1 }}
+                            className="w-full max-w-xl lg:w-3/4"
+                        >
+                            <h6 className="mb-2 text-2xl font-semibold tracking-tight text-transparent cursor-pointer bg-gradient-to-r from-pink-400 via-slate-500 to-purple-500 bg-clip-text hover:bg-gradient-to-r hover:from-purple-400 hover:via-slate-500 hover:to-pink-500 hover:bg-clip-text hover:tracking-tight hover:text-transparent">
+                                {project.title}
+                            </h6>
 
-              <a
-                href={project.link}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="flex items-center space-x-2 text-pink-500 hover:text-pink-200"
-              >
-                <FaExternalLinkAlt />
-                <span>Link</span>
-              </a>
+                            <a
+                                href={project.link}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="flex items-center space-x-2 text-pink-500 hover:text-pink-200"
+                            >
+                                <FaExternalLinkAlt />
+                                <span>Link</span>
+                            </a>
 
-              <p className="mb-4 text-neutral-400">{project.description}</p>
-              {project.technologies.map((tech, index) => (
-                <span
-                  key={index}
-                  className="mr-2 rounded-md bg-neutral-900 border-2 border-purple-900 px-2 py-1 text-sm font-medium text-purple-400 hover:cursor-pointer hover:bg-neutral-800 hover:text-purple-300"
-                >
-                  {tech}
-                </span>
-              ))}
-            </motion.div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+                            <p className="mb-4 text-neutral-400">
+                                {project.description}
+                            </p>
+                            {project.technologies.map((tech, index) => (
+                                <span
+                                    key={index}
+                                    className="px-2 py-1 mr-2 text-sm font-medium text-purple-400 border-2 border-purple-900 rounded-md bg-neutral-900 hover:cursor-pointer hover:bg-neutral-800 hover:text-purple-300"
+                                >
+                                    {tech}
+                                </span>
+                            ))}
+                        </motion.div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default Projects;
