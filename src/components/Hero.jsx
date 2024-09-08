eb981fe1f349133e92517add15e4b8027a6e4a2c
Kanad Shee <codewizardkanad@gmail.com>
2024-09-08 13:54:17 +0530

    Added motion to main heading

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,12 +19,12 @@ const container = (delay) => ({
 
 const Hero = () => {
   return (
-    <div className="border-b border-neutral-900 pb-4 lg:mb-35">
+    <div className="pb-4 border-b border-neutral-900 lg:mb-35">
       <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2">
           <div className="flex flex-col items-center lg:items-start">
             <motion.h1
-              variants={container(0)}
+              variants={container(0.3)}
               initial="hidden"
               animate="visible"
               className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl"
@@ -35,7 +35,7 @@ const Hero = () => {
               variants={container(0.5)}
               initial="hidden"
               animate="visible"
-              className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent"
+              className="text-3xl tracking-tight text-transparent bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text"
             >
               Full Stack Developer
             </motion.span>
