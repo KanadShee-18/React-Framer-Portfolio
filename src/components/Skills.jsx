4c2773dfffa55903cc90bcc2bb1405176a56c90c
Kanad Shee <codewizardkanad@gmail.com>
2024-09-12 23:51:58 +0530

    Added translate to skills section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -17,12 +17,12 @@ const skillsData = [
 
 const Skills = () => {
   return (
-    <div className="container mx-auto py-12 px-4">
+    <div className="container px-4 py-12 mx-auto">
       <motion.h2
         whileInView={{ opacity: 1, y: 0 }}
         initial={{ opacity: 0, y: 100 }}
         transition={{ duration: 1.5 }}
-        className="my-20 text-center text-5xl bg-gradient-to-r from-blue-800 via-blue-300 to-blue-200 bg-clip-text tracking-tight text-transparent pb-2"
+        className="pb-2 my-20 text-5xl tracking-tight text-center text-transparent bg-gradient-to-r from-blue-800 via-blue-300 to-blue-200 bg-clip-text"
       >
         Skills
       </motion.h2>
@@ -38,10 +38,10 @@ const Skills = () => {
             initial={{ opacity: 0, x: 100 }}
             transition={{ duration: 1, delay: 0.3 }}
             key={index}
-            className="m-4 w-full md:w-1/2 lg:w-1/4 flex flex-col"
+            className="flex flex-col w-full m-4 md:w-1/2 lg:w-1/4"
           >
-            <div className="flex-grow bg-gradient-to-b from-gray-800 to-gray-700 text-gray-400 rounded-lg p-6 transition-all duration-300 ease-in-out cursor-pointer shadow-lg shadow-neutral-500/50">
-              <h3 className="text-xl font-semibold text-pink-500 mb-2">
+            <div className="flex-grow p-6 text-gray-400 transition-all duration-300 ease-in-out rounded-lg shadow-lg cursor-pointer bg-gradient-to-b from-gray-800 to-gray-700 shadow-neutral-500/50 hover:-translate-y-2">
+              <h3 className="mb-2 text-xl font-semibold text-pink-500">
                 {skill.category}
               </h3>
               <p className="text-blue-300">{skill.description}</p>
